ef0b727a0f32d1bb33d80fb05cf094d31703d797
yoniwitz <jonhirsh39@gmail.com>
2021-11-08 08:08:22 -0500

    added exception handling

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -30,6 +30,7 @@ namespace Application
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
 
                 if (activity == null) return null;
+
                 _mapper.Map(request.Activity, activity);
 
                 var result = await _context.SaveChangesAsync() > 0;
